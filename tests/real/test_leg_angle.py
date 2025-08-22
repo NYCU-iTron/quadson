107c174f1db161a5790e1386480367254c924d29
zebra314 <kidalfonso@gmail.com>
2025-08-22 17:22:54 +0800

    Update leg name

--- tests/real/test_leg_angle.py
+++ tests/real/test_leg_angle.py
@@ -4,7 +4,7 @@ import logging
 import numpy as np
 from common.logging_config import setup_logging
 from real.motor_manager import MotorManager
-from real.leg import Leg
+from real.leg import Leg, LegName
 
 if __name__ == '__main__':
     setup_logging()
@@ -16,18 +16,18 @@ if __name__ == '__main__':
         logger.critical(f"Failed to initialize MotorManager: {e}", exc_info=True)
         exit(1)
     
-    lg = Leg(0, motor_manager)
+    leg = Leg(LegName.FRONT_LEFT, motor_manager)
 
     try:
         # ----------------------------------- Zero ----------------------------------- #
-        lg.enable_torque(True)
-        lg.set_control_mode(1)
-        lg.set_motor_omegas([None, np.pi, np.pi])
+        leg.enable_torque(True)
+        leg.set_control_mode(1)
+        leg.set_motor_omegas([None, np.pi, np.pi])
 
         lg_zero_states = [False, False, False]
         done = [None, False, False]
         while True:
-            lg_zero_states = lg.get_zero_states()
+            lg_zero_states = leg.get_zero_states()
 
             if done == [None, True, True]:
                 logger.info("All motors are zeroed, exiting loop.")
@@ -35,23 +35,23 @@ if __name__ == '__main__':
 
             if lg_zero_states[1] == True and not done[1]:
                 logger.info("Motor 1 is zeroed")
-                lg.set_motor_omegas([None, 0, None])
+                leg.set_motor_omegas([None, 0, None])
                 done[1] = True
             
             if lg_zero_states[2] == True and not done[2]:
                 logger.info("Motor 2 is zeroed")
-                lg.set_motor_omegas([None, None, 0])
+                leg.set_motor_omegas([None, None, 0])
                 done[2] = True
 
             logger.info(f"Zero states: {lg_zero_states}")
             time.sleep(0.05)
 
-        # lg.set_control_mode(0)
-        # angle = motor_manager.get_motor_angle(1)
-        # logger.info(f"Motor 1 deg: {np.rad2deg(angle)}")
-
         # -------------------------------- User Input -------------------------------- #
+        leg.set_control_mode(0)
         while True:
+            curret_angles = leg.get_motor_angles()
+            logger.info(f"Motor angles are at : {curret_angles}")
+
             logger.info("Waiting for user input to continue...")
             user_input = input("Press Enter to continue to motor angles...")
 
@@ -61,9 +61,8 @@ if __name__ == '__main__':
                 logger.error("Invalid input format. Please enter three integers separated by spaces.")
                 continue
 
-            lg.set_motor_angles([0, np.radians(b), np.radians(c)])
-            logger.info(f"Set motor angles to: 0, {b}, {c}")
-
+            leg.set_motor_angles([0, np.radians(b), np.radians(c)])
+            logger.info(f"Set motor angles to: 0, {np.radians(b)}, {np.radians(b)}")
 
             time.sleep(0.05)
 
@@ -75,7 +74,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     finally:
-        lg.stop_leg()
+        leg.stop_leg()
         time.sleep(0.05)
         motor_manager.shutdown()
         logger.info("Done")
